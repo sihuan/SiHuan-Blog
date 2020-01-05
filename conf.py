54eb61c8bb83b152ee1ab95fb42f0488325079e1
Si-Huan <if.nicer@gmail.com>
2020-01-05 15:47:52 +0800

    blog begin

---
+++ conf.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""SiHuan博客构建配置文件
+"""
+
+# For Maverick
+site_prefix = "/SiHuan-Blog/"
+template = {
+    "name": "Galileo",
+    "type": "git",
+    "url": "https://github.com/Si-Huan/Maverick-Theme-Galileo.git",
+    "branch": "latest"
+}
+source_dir = "../src/"
+build_dir = "../dist/"
+index_page_size = 10
+archives_page_size = 20
+enable_jsdelivr = {
+    "enabled": True,
+    "repo": "Si-Huan/SiHuan-Blog"
+}
+
+# 站点设置
+site_name = "SiHuan's Blog"
+site_logo = "${static_prefix}logo.png"
+site_build_date = "2020-0-0T0:0+08:00"
+author = "SiHuan"
+email = "sihuan@sakuya.love"
+author_homepage = "https://www.sakuya.love"
+description = "道阻且长，君子思还。"
+key_words = ['SiHuan', '思还', 'Blog', '博客', 'GitBlog']
+language = 'zh-CN'
+external_links = [
+    {
+        "name": "SiHuan-Wiki",
+        "url": "http://wiki.sakuya.love",
+        "brief": "我的知识积累。"
+    }
+]
+nav = [
+    {
+        "name": "首页",
+        "url": "${site_prefix}",
+        "target": "_self"
+    },
+    {
+        "name": "归档",
+        "url": "${site_prefix}archives/",
+        "target": "_self"
+    },
+    {
+        "name": "关于",
+        "url": "${site_prefix}about/",
+        "target": "_self"
+    }
+]
+
+social_links = [
+    {
+        "name": "Telegram",
+        "url": "https://t.me/Si_Huan",
+        # "icon": "gi gi-telegram"
+    },
+    {
+        "name": "GitHub",
+        "url": "https://github.com/Si-Huan",
+        # "icon": "gi gi-github"
+    }
+    # ,
+    # {
+    #     "name": "QQ",
+    #     "url": "https://weibo.com/5245109677/",
+    #     "icon": "gi gi-twitter"
+    # }
+]
+
+head_addon = r'''
+<meta http-equiv="x-dns-prefetch-control" content="on">
+<link rel="dns-prefetch" href="//cdn.jsdelivr.net" />
+'''
+
+footer_addon = ''
+
+body_addon = ''
